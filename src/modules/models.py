3441db9bb7ab5d303ddca36be8a3d8d52c48e183
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2024-02-11 11:59:40 +0100

    Pylint models.py.

--- src/modules/models.py
+++ src/modules/models.py
@@ -5,10 +5,10 @@ from typing import Tuple
 import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
+from keras.layers import GRU, LSTM, Conv1D, Dense, Flatten
+from keras.models import Sequential
+from keras.optimizers.legacy import Adam
 from sklearn.model_selection import TimeSeriesSplit
-from tensorflow.keras.layers import GRU, LSTM, Conv1D, Dense, Flatten
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers.legacy import Adam
 
 
 @dataclass(slots=True)
@@ -19,13 +19,13 @@ class Timeseries:
     n_features: int
     epochs: int
     company: str
-    model_Conv1D: object = None
-    model_GRU: object = None
-    model_LSTM: object = None
+    model_conv1d: Sequential = None
+    model_gru: Sequential = None
+    model_lstm: Sequential = None
 
     def models(self) -> None:
-        self.model_Conv1D = Sequential()
-        self.model_Conv1D.add(
+        self.model_conv1d = Sequential()
+        self.model_conv1d.add(
             Conv1D(
                 filters=64,
                 kernel_size=3,
@@ -33,114 +33,114 @@ class Timeseries:
                 input_shape=(self.n_steps_in, self.n_features),
             )
         )
-        self.model_Conv1D.add(Conv1D(filters=64, kernel_size=3, activation="relu"))
-        self.model_Conv1D.add(Flatten())
-        self.model_Conv1D.add(Dense(self.n_steps_out))
-        self.model_Conv1D.compile(
+        self.model_conv1d.add(Conv1D(filters=64, kernel_size=3, activation="relu"))
+        self.model_conv1d.add(Flatten())
+        self.model_conv1d.add(Dense(self.n_steps_out))
+        self.model_conv1d.compile(
             optimizer=Adam(learning_rate=0.0001), loss="mse", metrics=["accuracy"]
         )
 
-        self.model_GRU = Sequential()
-        self.model_GRU.add(
+        self.model_gru = Sequential()
+        self.model_gru.add(
             GRU(
                 200,
                 return_sequences=True,
                 input_shape=(self.n_steps_in, self.n_features),
             )
         )
-        self.model_GRU.add(GRU(200, return_sequences=True))
-        self.model_GRU.add(GRU(200, return_sequences=True))
-        self.model_GRU.add(GRU(200))
-        self.model_GRU.add(Dense(self.n_steps_out))
-        self.model_GRU.compile(
+        self.model_gru.add(GRU(200, return_sequences=True))
+        self.model_gru.add(GRU(200, return_sequences=True))
+        self.model_gru.add(GRU(200))
+        self.model_gru.add(Dense(self.n_steps_out))
+        self.model_gru.compile(
             optimizer=Adam(learning_rate=0.0001), loss="mse", metrics=["accuracy"]
         )
 
-        self.model_LSTM = Sequential()
-        self.model_LSTM.add(
+        self.model_lstm = Sequential()
+        self.model_lstm.add(
             LSTM(
                 200,
                 return_sequences=True,
                 input_shape=(self.n_steps_in, self.n_features),
             )
         )
-        self.model_LSTM.add(LSTM(200, return_sequences=True))
-        self.model_LSTM.add(LSTM(200, return_sequences=True))
-        self.model_LSTM.add(LSTM(200))
-        self.model_LSTM.add(Dense(self.n_steps_out))
-        self.model_LSTM.compile(
+        self.model_lstm.add(LSTM(200, return_sequences=True))
+        self.model_lstm.add(LSTM(200, return_sequences=True))
+        self.model_lstm.add(LSTM(200))
+        self.model_lstm.add(Dense(self.n_steps_out))
+        self.model_lstm.compile(
             optimizer=Adam(learning_rate=0.0001), loss="mse", metrics=["accuracy"]
         )
 
-    def train_models(self, X_train: np.array, y_train: list) -> None:
-        scores_LSTM = []
-        scores_Conv1D = []
-        scores_GRU = []
+    def train_models(self, x_train: np.array, y_train: list) -> None:
+        scores_lstm = []
+        scores_conv1d = []
+        scores_gru = []
 
-        timefold = TimeSeriesSplit(self.n_splits).split(X_train, y_train)
+        timefold = TimeSeriesSplit(self.n_splits).split(x_train, y_train)
         for k, (train, test) in enumerate(timefold):
-            self.model_LSTM.fit(
-                X_train[train],
+            self.model_lstm.fit(
+                x_train[train],
                 y_train[train],
                 epochs=self.epochs,
                 verbose=0,
                 validation_split=0.3,
             )
-            score_LSTM = self.model_LSTM.evaluate(
-                X_train[test],
+            score_lstm = self.model_lstm.evaluate(
+                x_train[test],
                 y_train[test],
                 verbose=0,
             )
-            scores_LSTM.append(score_LSTM)
+            scores_lstm.append(score_lstm)
             print(
                 "LSTM - Fold: %2d, Acc.: %.3f, Loss: %.3f"
-                % (k + 1, score_LSTM[1], score_LSTM[0])
+                % (k + 1, score_lstm[1], score_lstm[0])
             )
 
-            self.model_Conv1D.fit(
-                X_train[train],
+            self.model_conv1d.fit(
+                x_train[train],
                 y_train[train],
                 epochs=self.epochs,
                 verbose=0,
                 validation_split=0.3,
             )
-            score_Conv1D = self.model_Conv1D.evaluate(
-                X_train[test],
+            score_conv1d = self.model_conv1d.evaluate(
+                x_train[test],
                 y_train[test],
                 verbose=0,
             )
-            scores_Conv1D.append(score_Conv1D)
+            scores_conv1d.append(score_conv1d)
             print(
                 "Conv1D - Fold: %2d, Acc.: %.3f, Loss: %.3f"
-                % (k + 1, score_Conv1D[1], score_Conv1D[0])
+                % (k + 1, score_conv1d[1], score_conv1d[0])
             )
 
-            self.model_GRU.fit(
-                X_train[train],
+            self.model_gru.fit(
+                x_train[train],
                 y_train[train],
                 epochs=self.epochs,
                 verbose=0,
                 validation_split=0.3,
             )
-            score_GRU = self.model_GRU.evaluate(
-                X_train[test],
+            score_gru = self.model_gru.evaluate(
+                x_train[test],
                 y_train[test],
                 verbose=0,
             )
-            scores_GRU.append(score_GRU)
+            scores_gru.append(score_gru)
             print(
                 "GRU - Fold: %2d, Acc.: %.4f, Loss: %.4f"
-                % (k + 1, score_GRU[1], score_GRU[0])
+                % (k + 1, score_gru[1], score_gru[0])
             )
         if (
             os.path.isfile("./models/model_LSTM_" + str(self.company) + ".keras")
             is False
         ):
-            self.model_LSTM.save("./models/model_LSTM_" + str(self.company) + ".keras")
-            self.model_Conv1D.save(
+            self.model_lstm.save("./models/model_LSTM_" + str(self.company) + ".keras")
+            self.model_conv1d.save(
                 "./models/model_Conv1D_" + str(self.company) + ".keras"
             )
-            self.model_GRU.save("./models/model_GRU_" + str(self.company) + ".keras")
+            self.model_gru.save("./models/model_GRU_" + str(self.company) + ".keras")
 
     def test_prediction_if_no_previous_models(
         self,
@@ -148,16 +148,16 @@ class Timeseries:
         df: pd.DataFrame,
         scaler: object,
     ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        yhat_LSTM = self.model_LSTM.predict(x_input, verbose=0)
-        yhat_Conv1D = self.model_Conv1D.predict(x_input, verbose=0)
-        yhat_GRU = self.model_GRU.predict(x_input, verbose=0)
+        yhat_lstm = self.model_lstm.predict(x_input, verbose=0)
+        yhat_conv1d = self.model_conv1d.predict(x_input, verbose=0)
+        yhat_gru = self.model_gru.predict(x_input, verbose=0)
 
-        mean_LSTM = np.mean(yhat_LSTM, axis=1)
-        mean_Conv1D = np.mean(yhat_Conv1D, axis=1)
-        mean_GRU = np.mean(yhat_GRU, axis=1)
+        mean_lstm = np.mean(yhat_lstm, axis=1)
+        mean_Conv1D = np.mean(yhat_conv1d, axis=1)
+        mean_GRU = np.mean(yhat_gru, axis=1)
 
-        mean_LSTM = scaler.inverse_transform(mean_LSTM.reshape(-1, 1)).reshape(
-            len(mean_LSTM),
+        mean_lstm = scaler.inverse_transform(mean_lstm.reshape(-1, 1)).reshape(
+            len(mean_lstm),
         )
         mean_Conv1D = scaler.inverse_transform(mean_Conv1D.reshape(-1, 1)).reshape(
             len(mean_Conv1D),
@@ -169,27 +169,27 @@ class Timeseries:
         df["Close"] = scaler.inverse_transform(
             df["Close"].to_numpy().reshape(-1, 1)
         ).reshape(old_df)
-        return mean_LSTM, mean_Conv1D, mean_GRU, df
+        return mean_lstm, mean_Conv1D, mean_GRU, df
 
     def test_prediction_if_previous_models(
         self,
         x_input: np.array,
         df: pd.DataFrame,
-        model_LSTM: object,
-        model_Conv1D: object,
-        model_GRU: object,
+        model_lstm: object,
+        model_conv1d: object,
+        model_gru: object,
         scaler: object,
     ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        yhat_LSTM = model_LSTM.predict(x_input, verbose=0)
-        yhat_Conv1D = model_Conv1D.predict(x_input, verbose=0)
-        yhat_GRU = model_GRU.predict(x_input, verbose=0)
+        yhat_lstm = model_lstm.predict(x_input, verbose=0)
+        yhat_conv1d = model_conv1d.predict(x_input, verbose=0)
+        yhat_gru = model_gru.predict(x_input, verbose=0)
 
-        mean_LSTM = np.mean(yhat_LSTM, axis=1)
-        mean_Conv1D = np.mean(yhat_Conv1D, axis=1)
-        mean_GRU = np.mean(yhat_GRU, axis=1)
+        mean_lstm = np.mean(yhat_lstm, axis=1)
+        mean_Conv1D = np.mean(yhat_conv1d, axis=1)
+        mean_GRU = np.mean(yhat_gru, axis=1)
 
-        mean_LSTM = scaler.inverse_transform(mean_LSTM.reshape(-1, 1)).reshape(
-            len(mean_LSTM),
+        mean_lstm = scaler.inverse_transform(mean_lstm.reshape(-1, 1)).reshape(
+            len(mean_lstm),
         )
         mean_Conv1D = scaler.inverse_transform(mean_Conv1D.reshape(-1, 1)).reshape(
             len(mean_Conv1D),
@@ -202,7 +202,7 @@ class Timeseries:
         df["Close"] = scaler.inverse_transform(
             df["Close"].to_numpy().reshape(-1, 1)
         ).reshape(old_df)
-        return mean_LSTM, mean_Conv1D, mean_GRU, df
+        return mean_lstm, mean_Conv1D, mean_GRU, df
 
     def figure(
         self,
